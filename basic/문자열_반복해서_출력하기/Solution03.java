c9fed31db541ba87774ad009919e2e8ad108b478
wu <so222234@gmail.com>
2024-10-09 00:43:52 +0900

    :sparkles: Solution '문자열 반복해서 출력하기'-기본 반복문 풀이

--- basic/문자열_반복해서_출력하기/Solution03.java
+++ basic/문자열_반복해서_출력하기/Solution03.java
@@ -7,5 +7,9 @@ public class Solution03 {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(str);
+        }
     }
 }
