a41ac238f01cb16d11887b7aa4664f1b48d9b689
wu <so222234@gmail.com>
2024-10-09 01:13:38 +0900

    :sparkles: Set Problem '대소문자_바꿔서_출력하기'

---
+++ basic/대소문자_바꿔서_출력하기/Solution04.java
@@ -0,0 +1,10 @@
+package basic.대소문자_바꿔서_출력하기;
+
+import java.util.Scanner;
+
+public class Solution04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+    }
+}
