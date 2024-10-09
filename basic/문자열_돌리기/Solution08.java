e826de46d3cdfba9a37b817002ef4251e9f9fb8e
wu <so222234@gmail.com>
2024-10-09 23:30:07 +0900

    :sparkles: Set Problem '문자열 돌리기'

---
+++ basic/문자열_돌리기/Solution08.java
@@ -0,0 +1,10 @@
+package basic.문자열_돌리기;
+
+import java.util.Scanner;
+
+public class Solution08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+    }
+}
