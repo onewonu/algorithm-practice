3b5fe8f7f9bba40f496fdae9e484477332108c92
wu <so222234@gmail.com>
2024-10-10 00:02:30 +0900

    :sparkles: Solution '홀짝 구분하기'-조건문

--- basic/홀짝_구분하기/Solution09.java
+++ basic/홀짝_구분하기/Solution09.java
@@ -6,5 +6,11 @@ public class Solution09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.printf("%d is even", n);
+        } else {
+            System.out.printf("%d is odd", n);
+        }
     }
 }
