6a9bf3532e8b4850044771a577d3eb5d8fdad755
wu <so222234@gmail.com>
2024-10-09 23:18:00 +0900

    :sparkles: Set Problem '문자열 붙여서 출력하기'

---
+++ basic/문자열_붙여서_출력하기/Solution07.java
@@ -0,0 +1,11 @@
+package basic.문자열_붙여서_출력하기;
+
+import java.util.Scanner;
+
+public class Solution07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+    }
+}
