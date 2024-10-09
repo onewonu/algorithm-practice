ce9a93e6d5094e302a8b55fc4cd873f97b048763
wu <so222234@gmail.com>
2024-10-10 00:17:07 +0900

    :sparkles: Set Problem '문자열 겹쳐쓰기'

---
+++ basic/문자열_겹쳐쓰기/Solution10.java
@@ -0,0 +1,14 @@
+package basic.문자열_겹쳐쓰기;
+
+import java.util.Scanner;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        // solution
+    }
+
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        return answer;
+    }
+}
