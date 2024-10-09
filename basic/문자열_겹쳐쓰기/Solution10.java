4741e80548fdd1cc383854fea80bb33e7217a1e1
wu <so222234@gmail.com>
2024-10-10 00:32:07 +0900

    :memo: Solution '문자열 겹쳐쓰기'-TODO 작성

--- basic/문자열_겹쳐쓰기/Solution10.java
+++ basic/문자열_겹쳐쓰기/Solution10.java
@@ -10,5 +10,9 @@ public class Solution10 {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
         return answer;
+
+        // TODO:
+        //  인덱스 전까지의 문자열을 추출하고, 그 부분을 덮어쓴다.
+        //  덮어쓴 부분의 끝에서 부터 나머지 부분을 합쳐 최종 문자열을 생성.
     }
 }
