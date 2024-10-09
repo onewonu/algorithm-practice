6a7185b44531fe85bc7389d1d57a6bf9dd0166d6
wu <so222234@gmail.com>
2024-10-09 23:52:34 +0900

    :sparkles: Set Problem '홀짝 구분하기'

---
+++ basic/홀짝_구분하기/Solution09.java
@@ -0,0 +1,10 @@
+package basic.홀짝_구분하기;
+
+import java.util.Scanner;
+
+public class Solution09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
