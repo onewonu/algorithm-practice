1a72a2d5f392ad6b1ac4b327d33061015466a14b
wu <so222234@gmail.com>
2024-10-13 01:33:33 +0900

    :memo: Set Problem '문자 리스트를 문자열로 변화하기'

---
+++ basic/문자_리스트를_문자열로_변환하기/Solution12.java
@@ -0,0 +1,14 @@
+package basic.문자_리스트를_문자열로_변환하기;
+
+public class Solution12 {
+    public static void main(String[] args) {
+        String[] arr1 = {"a", "b", "c"};
+        String solution = solution(arr1);
+        System.out.println("solution = " + solution);
+    }
+
+    public static String solution(String[] arr) {
+        String answer = "";
+        return answer;
+    }
+}
