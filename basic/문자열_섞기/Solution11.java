0e78244048bd3fb8cb93d3bf5d1ee8fb42e81992
wu <so222234@gmail.com>
2024-10-13 01:08:55 +0900

    :memo: Set Problem '문자열 섞기'

---
+++ basic/문자열_섞기/Solution11.java
@@ -0,0 +1,13 @@
+package basic.문자열_섞기;
+
+public class Solution11 {
+    public static void main(String[] args) {
+        String solution = solution("aaaaa", "bbbbb");
+        System.out.println("solution = " + solution);
+    }
+
+    public static String solution(String str1, String str2) {
+        String answer = "";
+        return answer;
+    }
+}
