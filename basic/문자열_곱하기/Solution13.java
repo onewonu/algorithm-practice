5133ca0fb63c73e99b624aceb70c5cd4d059a5a1
wu <so222234@gmail.com>
2024-10-13 01:51:44 +0900

    :memo: Set Problem '문자열 곱하기'

---
+++ basic/문자열_곱하기/Solution13.java
@@ -0,0 +1,15 @@
+package basic.문자열_곱하기;
+
+public class Solution13 {
+    public static void main(String[] args) {
+        Solution13 solution13 = new Solution13();
+
+        String solution = solution13.solution("stringstringstring", 3);
+        System.out.println("solution = " + solution);
+    }
+
+    public String solution(String my_string, int k) {
+        String answer = "";
+        return answer;
+    }
+}
