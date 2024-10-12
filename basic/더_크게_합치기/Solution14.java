113d4f7a71a7d73145ae39004a01323e236fc0a3
wu <so222234@gmail.com>
2024-10-13 02:06:38 +0900

    :memo: Set Problem '더 크게 합치기'

---
+++ basic/더_크게_합치기/Solution14.java
@@ -0,0 +1,15 @@
+package basic.더_크게_합치기;
+
+public class Solution14 {
+    public static void main(String[] args) {
+        Solution14 solution13 = new Solution14();
+
+        int solution = solution13.solution(9, 91);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int a, int b) {
+        int answer = 0;
+        return answer;
+    }
+}
