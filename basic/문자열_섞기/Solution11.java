119ce0830593590504abb217f6007fd2943c9560
wu <so222234@gmail.com>
2024-10-13 01:14:11 +0900

    :sparkles: Solution '문자열 섞기'-StringBuilder

--- basic/문자열_섞기/Solution11.java
+++ basic/문자열_섞기/Solution11.java
@@ -7,7 +7,13 @@ public class Solution11 {
     }
 
     public static String solution(String str1, String str2) {
-        String answer = "";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < str1.length(); i++) {
+            stringBuilder.append(str1.charAt(i));
+            stringBuilder.append(str2.charAt(i));
+        }
+
+        return stringBuilder.toString();
     }
 }
