8f79edfab4420bc6a12f2727559f9751dab25b28
wu <so222234@gmail.com>
2024-10-14 00:00:15 +0900

    :memo: Set Problem '두 수의 연산값 비교하기'

--- basic/더_크게_합치기/Solution14.java
+++ basic/더_크게_합치기/Solution14.java
@@ -2,9 +2,9 @@ package basic.더_크게_합치기;
 
 public class Solution14 {
     public static void main(String[] args) {
-        Solution14 solution13 = new Solution14();
+        Solution14 Solution14 = new Solution14();
 
-        int solution = solution13.solution(9, 91);
+        int solution = Solution14.solution(9, 91);
         System.out.println("solution = " + solution);
     }
 
---
+++ basic/두_수의_연산값_비교하기/Solution15.java
@@ -0,0 +1,15 @@
+package basic.두_수의_연산값_비교하기;
+
+public class Solution15 {
+    public static void main(String[] args) {
+        Solution15 solution15 = new Solution15();
+
+        int solution = solution15.solution(9, 91);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int a, int b) {
+        int answer = 0;
+        return answer;
+    }
+}
