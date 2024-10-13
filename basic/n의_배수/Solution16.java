0e4a6ce9b717c9739a401e4b51a77648d2a94411
wu <so222234@gmail.com>
2024-10-14 00:45:59 +0900

    :memo: Set Problem 'n의 배수'

---
+++ basic/n의_배수/Solution16.java
@@ -0,0 +1,15 @@
+package basic.n의_배수;
+
+public class Solution16 {
+    public static void main(String[] args) {
+        Solution16 solution16 = new Solution16();
+
+        int solution = solution16.solution(98, 2);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int num, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
