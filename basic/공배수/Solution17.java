c00600bdcf0bbdb7854f3719581a446d2dfc55f5
wu <so222234@gmail.com>
2024-10-14 22:23:57 +0900

    :memo: Set Problem '공배수'

---
+++ basic/공배수/Solution17.java
@@ -0,0 +1,15 @@
+package basic.공배수;
+
+public class Solution17 {
+    public static void main(String[] args) {
+        Solution17 solution17 = new Solution17();
+
+        int solution = solution17.solution(60, 2, 3);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        return answer;
+    }
+}
