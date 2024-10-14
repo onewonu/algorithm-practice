574e52bc1b2f623b1b1f86308e11a976a8183ddb
wu <so222234@gmail.com>
2024-10-14 22:55:15 +0900

    :memo: Set Problem '홀짝에 따라 다른 값 반환하기'

---
+++ basic/홀짝에_따라_다른_값_변환하기/Solution18.java
@@ -0,0 +1,19 @@
+package basic.홀짝에_따라_다른_값_변환하기;
+
+public class Solution18 {
+    public static void main(String[] args) {
+        Solution18 solution = new Solution18();
+        runTest(solution, 7);
+        runTest(solution, 10);
+    }
+
+    private static void runTest(Solution18 solution, int n) {
+        int result = solution.solution(n);
+        System.out.printf("solution(%d) = %d%n", n, result);
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
