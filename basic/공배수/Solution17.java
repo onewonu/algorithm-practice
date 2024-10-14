7d08c12ac7b9367556e01a8494909a05a27b021e
wu <so222234@gmail.com>
2024-10-14 22:48:10 +0900

    :sparkles: Solution '공배수'

--- basic/공배수/Solution17.java
+++ basic/공배수/Solution17.java
@@ -3,13 +3,18 @@ package basic.공배수;
 public class Solution17 {
     public static void main(String[] args) {
         Solution17 solution17 = new Solution17();
+        runTest(solution17, 60, 2, 3);
+        runTest(solution17, 55, 10, 5);
+    }
 
-        int solution = solution17.solution(60, 2, 3);
-        System.out.println("solution = " + solution);
+    private static void runTest(Solution17 solution, int number, int n, int m) {
+        int result = solution.solution(number, n, m);
+        System.out.printf("solution(%d, %d, %d) = %d%n", number, n, m, result);
     }
 
     public int solution(int number, int n, int m) {
         int answer = 0;
+        if (number % n == 0 && number % m == 0) { answer = 1; }
         return answer;
     }
 }
