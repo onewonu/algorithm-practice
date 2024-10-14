eaa380c6deeb8ee127ae0ecdb239159cf1e47a43
wu <so222234@gmail.com>
2024-10-14 23:13:29 +0900

    :sparkles: Solution '홀짝에 따라 다른 값 반환하기'-기본 반복문

--- basic/홀짝에_따라_다른_값_변환하기/Solution18.java
+++ basic/홀짝에_따라_다른_값_변환하기/Solution18.java
@@ -14,6 +14,17 @@ public class Solution18 {
 
     public int solution(int n) {
         int answer = 0;
+
+        if (n % 2 == 1) {
+            for (int i = 1; i <= n; i += 2) {
+                answer += i;
+            }
+        } else if (n % 2 == 0) {
+            for (int i = 2; i <= n; i += 2) {
+                answer += i * i;
+            }
+        }
+
         return answer;
     }
 }
