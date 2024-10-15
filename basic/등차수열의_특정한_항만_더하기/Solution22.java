89d6e62651a190759a7db6e9194b3def6dbc2be0
wu <so222234@gmail.com>
2024-10-16 00:40:23 +0900

    :memo: Set Problem '등차수열의 특정한 항만 더하기'

---
+++ basic/등차수열의_특정한_항만_더하기/Solution22.java
@@ -0,0 +1,21 @@
+package basic.등차수열의_특정한_항만_더하기;
+
+import java.util.Arrays;
+
+public class Solution22 {
+    public static void main(String[] args) {
+        Solution22 solution = new Solution22();
+        runTest(solution, 3, 4, new boolean[]{true, false, false, true, true});
+        runTest(solution, 7, 1, new boolean[]{false, false, false, true, false, false, false});
+    }
+
+    private static void runTest(Solution22 solution, int a, int d, boolean[] included) {
+        int result = solution.solution(a, d, included);
+        System.out.printf("solution(%d, %d, %s) = %s%n", a, d, Arrays.toString(included), result);
+    }
+
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        return answer;
+    }
+}
