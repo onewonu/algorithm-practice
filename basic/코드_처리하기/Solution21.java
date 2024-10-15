1a4da9f78b7469ae645cb376d09d1844cd3ead6f
wu <so222234@gmail.com>
2024-10-15 23:32:17 +0900

    :sparkles: Solution '코드 처리하기'-반복문, 조건문

--- basic/코드_처리하기/Solution21.java
+++ basic/코드_처리하기/Solution21.java
@@ -13,6 +13,32 @@ public class Solution21 {
 
     public String solution(String code) {
         String answer = "";
+
+        int mode = 0;
+
+        // TODO: 조건의 간소화 필요.
+        for (int i = 0; i < code.length(); i++) {
+            if (mode == 0) {
+                if (code.charAt(i) != '1') {
+                    if (i % 2 == 0) {
+                        answer += code.charAt(i);
+                    }
+                } else {
+                    mode = 1;
+                }
+            } else {
+                if (code.charAt(i) != '1') {
+                    if (i % 2 != 0) {
+                        answer += code.charAt(i);
+                    }
+                } else {
+                    mode = 0;
+                }
+            }
+        }
+
+        if (answer.isEmpty()) { answer = "EMPTY"; }
+
         return answer;
     }
 }
