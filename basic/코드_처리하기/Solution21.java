14d246667074a4c102caaa069f44431baff0e05e
wu <so222234@gmail.com>
2024-10-15 23:02:05 +0900

    :memo: Set Problem '코드 처리하기'

---
+++ basic/코드_처리하기/Solution21.java
@@ -0,0 +1,18 @@
+package basic.코드_처리하기;
+
+public class Solution21 {
+    public static void main(String[] args) {
+        Solution21 solution = new Solution21();
+        runTest(solution, "abc1abc1abc");
+    }
+
+    private static void runTest(Solution21 solution, String code) {
+        String result = solution.solution(code);
+        System.out.printf("solution(%s) = %s%n", code, result);
+    }
+
+    public String solution(String code) {
+        String answer = "";
+        return answer;
+    }
+}
