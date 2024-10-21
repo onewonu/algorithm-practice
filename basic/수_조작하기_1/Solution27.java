6bc5ac2ef2cd853a1f71e23b346175ff80aa21b5
wu <so222234@gmail.com>
2024-10-21 23:17:01 +0900

    :memo: Set Problem '수 조작하기 1'

---
+++ basic/수_조작하기_1/Solution27.java
@@ -0,0 +1,18 @@
+package basic.수_조작하기_1;
+
+public class Solution27 {
+    public static void main(String[] args) {
+        Solution27 solution = new Solution27();
+        runTest(solution, 0, "wsdawsdassw" );
+    }
+
+    private static void runTest(Solution27 solution, int n, String control) {
+        int result = solution.solution(n, control);
+        System.out.printf("solution(%d, %s) = %s%n", n, control, result);
+    }
+
+    public int solution(int n, String control) {
+        int answer = 0;
+        return answer;
+    }
+}
