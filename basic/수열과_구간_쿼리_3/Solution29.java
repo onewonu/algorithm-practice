f73a8ef52acaf94301042c5a1f4b9cc6a4377cb4
wu <so222234@gmail.com>
2024-10-22 23:13:11 +0900

    :memo: Set Problem '수열과 구간 쿼리 3'

---
+++ basic/수열과_구간_쿼리_3/Solution29.java
@@ -0,0 +1,20 @@
+package basic.수열과_구간_쿼리_3;
+
+import java.util.Arrays;
+
+public class Solution29 {
+    public static void main(String[] args) {
+        Solution29 solution = new Solution29();
+        runTest(solution, new int[]{0, 1, 2, 3, 4}, new int[][]{{0, 3}, {1, 2}, {1, 4}});
+    }
+
+    private static void runTest(Solution29 solution, int[] arr, int[][] queries) {
+        int[] result = solution.solution(arr, queries);
+        System.out.printf("solution(%s, %s) = %s%n", Arrays.toString(arr), Arrays.deepToString(queries), Arrays.toString(result));
+    }
+
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+}
