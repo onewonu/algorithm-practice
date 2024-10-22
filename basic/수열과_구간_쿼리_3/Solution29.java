04307ca7dc5124ca7c7d7df1977812d6e2c206d1
wu <so222234@gmail.com>
2024-10-23 00:53:02 +0900

    :sparkles: Solution '수열과 구간 쿼리 3'-배열에서 두 값을 교환(temp)

--- basic/수열과_구간_쿼리_3/Solution29.java
+++ basic/수열과_구간_쿼리_3/Solution29.java
@@ -14,7 +14,16 @@ public class Solution29 {
     }
 
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return answer;
+
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return arr;
     }
 }
