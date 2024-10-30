481c6b58c15ca31615da7a78c4d45b51ad76f190
wu <so222234@gmail.com>
2024-10-31 01:02:36 +0900

    :memo: Set Problem '콜라츠 수열 만들기'

---
+++ basic/콜라츠_수열_만들기/Solution34.java
@@ -0,0 +1,20 @@
+package basic.콜라츠_수열_만들기;
+
+import java.util.Arrays;
+
+public class Solution34 {
+    public static void main(String[] args) {
+        Solution34 solution = new Solution34();
+        runTest(solution, 10);
+    }
+
+    private static void runTest(Solution34 solution, int n) {
+        int[] result = solution.solution(n);
+        System.out.printf("solution(%d) = %s%n", n, Arrays.toString(result));
+    }
+
+    public int[] solution(int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
