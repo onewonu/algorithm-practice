bfe0635ff5df509df977b4bce8f235537c16d2b6
wu <so222234@gmail.com>
2024-10-31 01:03:33 +0900

    :sparkles: Solution '콜라츠 수열 만들기'-do while, 마지막 1 추가

--- basic/콜라츠_수열_만들기/Solution34.java
+++ basic/콜라츠_수열_만들기/Solution34.java
@@ -1,6 +1,8 @@
 package basic.콜라츠_수열_만들기;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution34 {
     public static void main(String[] args) {
@@ -14,7 +16,20 @@ public class Solution34 {
     }
 
     public int[] solution(int n) {
-        int[] answer = {};
-        return answer;
+        List<Integer> arrayList = new ArrayList<>();
+
+        do {
+            arrayList.add(n);
+
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        } while (n != 1);
+
+        arrayList.add(1);
+
+        return arrayList.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
