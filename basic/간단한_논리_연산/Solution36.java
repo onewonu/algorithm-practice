4af04271bef91e240389ec592835dff91be2ffa2
wu <so222234@gmail.com>
2024-11-05 00:24:05 +0900

    :sparkles: Solution '간단한 논리 연산'

--- basic/간단한_논리_연산/Solution36.java
+++ basic/간단한_논리_연산/Solution36.java
@@ -1,8 +1,5 @@
 package basic.간단한_논리_연산;
 
-import java.util.Arrays;
-import java.util.Stack;
-
 public class Solution36 {
     public static void main(String[] args) {
         Solution36 solution = new Solution36();
@@ -17,7 +14,6 @@ public class Solution36 {
     }
 
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
-        return answer;
+        return (x1 || x2) && (x3 || x4);
     }
 }
