cea821eca8aa047c7772226e369d1b37852d1c43
wu <so222234@gmail.com>
2024-11-05 01:27:46 +0900

    :construction: Work in Progress '주사위 게임 3'-Hash

--- basic/주사위_게임_3/Solution37.java
+++ basic/주사위_게임_3/Solution37.java
@@ -1,7 +1,6 @@
 package basic.주사위_게임_3;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.HashMap;
 
 public class Solution37 {
     public static void main(String[] args) {
@@ -19,7 +18,34 @@ public class Solution37 {
     }
 
     public int solution(int a, int b, int c, int d) {
-        int answer = 0;
-        return answer;
+
+        int[] dice = {a, b, c, d};
+
+        HashMap<Integer, Integer> countDice = new HashMap<>();
+
+        for (int number : dice) {
+            countDice.put(number, countDice.getOrDefault(number, 0) + 1);
+            System.out.println(countDice);
+        }
+
+        // countDice.size() == 1
+
+        // TODO: countDice.size() == 2 일 경우, (세 주사위에서 나온 숫자가 p로 같고) 와 (두 개씩 같은 값의 사이즈가 같다) 의 결과가 같음
+        /*
+            {4=1}
+            {1=1, 4=1}
+            {1=1, 4=2}
+            {1=1, 4=3}
+            solution(4, 1, 4, 4) = 0
+            {6=1}
+            {3=1, 6=1}
+            {3=2, 6=1}
+            {3=2, 6=2}
+            solution(6, 3, 3, 6) = 0
+        */
+
+        // countDice.size() == 3
+
+        return 0;
     }
 }
