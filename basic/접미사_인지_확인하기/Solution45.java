0e4f8275d7d7995a2ee84d2a4ecd08c0bd8c54ed
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-20 17:32:34 +0900

    :construction: Resolving '접미사인지 확인하기'

--- basic/접미사_인지_확인하기/Solution45.java
+++ basic/접미사_인지_확인하기/Solution45.java
@@ -18,6 +18,19 @@ public class Solution45 {
 
     public int solution(String my_string, String is_suffix) {
         int answer = 0;
+
+        String[] suffix = new String[my_string.length()];
+        for (int i = 0; i < my_string.length(); i++) {
+            suffix[i] = my_string.substring(i);
+        }
+
+        for (String s : suffix) {
+            if (s.equals(is_suffix)) {
+                answer = 1;
+                break;
+            }
+        }
+
         return answer;
     }
 }
