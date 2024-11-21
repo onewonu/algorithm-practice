c61e2af6a8a2e748f6c1e710657748a1ba5ed7ab
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-21 11:23:10 +0900

    :sparkles: Solution '문자열 앞의 n글자'

--- basic/문자열의_앞의_n글자/Solution46.java
+++ basic/문자열의_앞의_n글자/Solution46.java
@@ -13,7 +13,6 @@ public class Solution46 {
     }
 
     public String solution(String my_string, int n) {
-        String answer = "";
-        return answer;
+        return my_string.substring(0, n);
     }
 }
