d706b2c07e58302a1bba5a7a9150d0f48e411b61
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-25 14:11:07 +0900

    :memo: Set Problem 'QR Code'

---
+++ basic/qr_code/Solution50.java
@@ -0,0 +1,19 @@
+package basic.qr_code;
+
+public class Solution50 {
+    public static void main(String[] args) {
+        Solution50 solution = new Solution50();
+        runTest(solution, 3, 1, "qjnwezgrpirldywt");
+        runTest(solution, 1, 0, "programmers");
+    }
+
+    private static void runTest(Solution50 solution, int q, int r, String code) {
+        String result = solution.solution(q, r, code);
+        System.out.printf("solution(%d %d %s) = %s%n", q, r, code, result);
+    }
+
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        return answer;
+    }
+}
