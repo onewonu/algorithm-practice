280a0dde3a5ea4167770c1703eace9a83654c175
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-26 17:16:34 +0900

    :sparkles: Solution '가까운 1 찾기'

--- basic/가까운_1_찾기/Solution55.java
+++ basic/가까운_1_찾기/Solution55.java
@@ -16,7 +16,10 @@ public class Solution55 {
     }
 
     public int solution(int[] arr, int idx) {
-        int answer = 0;
-        return answer;
+        for (int i = idx; i < arr.length; i++) {
+            if (arr[i] == 1) return i;
+        }
+
+        return -1;
     }
 }
