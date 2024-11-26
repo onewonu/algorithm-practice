1bc2275d872f2448f8564a8b463682eee953c1f5
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-26 15:17:13 +0900

    :recycle: Solution '카운트 다운'-인덱스 변수 제거 및 고정 배열 크기 만큼 반, 배열 값 계산 방법 변경

--- basic/카운트_다운/Solution54.java
+++ basic/카운트_다운/Solution54.java
@@ -15,8 +15,7 @@ public class Solution54 {
 
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[start_num - end_num + 1];
-        int index = 0;
-        for (int i = start_num; i >= end_num; i--) answer[index++] = i;
+        for (int i = 0; i < answer.length; i++) answer[i] = start_num - i;
         return answer;
     }
 }
