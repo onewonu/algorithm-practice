61c772347996c602a2bfcc3bd2915ee30e53410f
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-27 14:44:40 +0900

    :sparkles: Solution '첫 번쨰로 나오는 음수'

--- basic/첫_번쨰로_나오는_음수/Solution57.java
+++ basic/첫_번쨰로_나오는_음수/Solution57.java
@@ -15,7 +15,10 @@ public class Solution57 {
     }
 
     public int solution(int[] num_list) {
-        int answer = 0;
-        return answer;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) return i;
+        }
+
+        return -1;
     }
 }
