047283d0e2b0175ba175491814ca7b673688b83f
Wonwoo Seo <wonwooseo2@gmail.com>
2024-11-27 17:46:38 +0900

    :construction: Solution '2의 영역'

--- basic/_2의_영역/Solution59.java
+++ basic/_2의_영역/Solution59.java
@@ -17,7 +17,31 @@ public class Solution59 {
     }
 
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        // TODO: 처음으로 2를 만났을 때 startIndex 를 설정.
+        // TODO: 2를 만날 때마다 endIndex 를 갱신.
+        // TODO: 만약 startIndex 와 endIndex 가 갱신되지 않았다면, 배열에 2가 없는 것이므로 [-1]을 반환.
+
+        int start = -1;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                if (start == -1) {
+                    start = i;
+                }
+                end = i;
+            }
+        }
+
+        if (start == -1) {
+            return new int[]{-1};
+        }
+
+        int[] answer = new int[end - start + 1];
+        for (int i = start; i <= end; i++) {
+            answer[i - start] = arr[i];
+        }
+
         return answer;
     }
 }
