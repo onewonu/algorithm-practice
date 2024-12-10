48ff7d79ac20f147b0c0fa58feaa1e17ef0a9747
Wonwoo Seo <wonwooseo2@gmail.com>
2024-12-10 17:43:40 +0900

    :memo: Set Problem '순서 바꾸기'

---
+++ basic/순서_바꾸기/Solution62.java
@@ -0,0 +1,21 @@
+package basic.순서_바꾸기;
+
+import java.util.Arrays;
+
+public class Solution62 {
+    public static void main(String[] args) {
+        Solution62 solution = new Solution62();
+        runTest(solution, new int[] {2, 1, 6}, 1);
+        runTest(solution, new int[] {5, 2, 1, 7, 5}, 3);
+    }
+
+    private static void runTest(Solution62 solution, int[] num_list, int n) {
+        int[] result = solution.solution(num_list, n);
+        System.out.printf("solution(%s %d) = %s%n", Arrays.toString(num_list), n, Arrays.toString(result));
+    }
+
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
