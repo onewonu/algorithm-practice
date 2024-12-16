a617feaeeac687dbf67bfa77934bf12497f38f0e
wu <so222234@gmail.com>
2024-12-16 21:39:59 +0900

    :memo: Fix Set Problem 'n개 간격의 원소들'

--- basic/n개_간격의_원소들/Solution65.java
+++ basic/n개_간격의_원소들/Solution65.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Solution65 {
     public static void main(String[] args) {
         Solution65 solution = new Solution65();
-        runTest(solution, new int[] {2, 1, 6}, 1);
-        runTest(solution, new int[] {5, 2, 1, 7, 5}, 3);
+        runTest(solution, new int[] {4, 2, 6, 1, 7, 6}, 2);
+        runTest(solution, new int[] {4, 2, 6, 1, 7, 6}, 4);
     }
 
     private static void runTest(Solution65 solution, int[] num_list, int n) {
