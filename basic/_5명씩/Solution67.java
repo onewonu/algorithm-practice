df6457cb8380396fce752ecf3e58ad96b972c1da
wu <so222234@gmail.com>
2024-12-18 00:22:09 +0900

    :construction: Resolving '5명씩'-요구사항의 반환에 집중하여 간단한 인덱스 설정

--- basic/_5명씩/Solution67.java
+++ basic/_5명씩/Solution67.java
@@ -13,8 +13,12 @@ public class Solution67 {
         System.out.printf("solution(%s) = %s%n", Arrays.toString(names), Arrays.toString(result));
     }
 
+    private static final int GROUP_SIZE = 5;
+
     public String[] solution(String[] names) {
-        String[] answer = {};
+        int size = (names.length + GROUP_SIZE - 1) / GROUP_SIZE;
+        String[] answer = new String[size];
+        for (int i = 0; i < size; i++) answer[i] = names[i * GROUP_SIZE];
         return answer;
     }
 }
