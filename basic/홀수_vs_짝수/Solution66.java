ba981dbc83b1c612d12056b880a592b41eaf257f
wu <so222234@gmail.com>
2024-12-17 23:39:13 +0900

    :sparkles: Solution '홀수 vs 짝수'-Math.max

--- basic/홀수_vs_짝수/Solution66.java
+++ basic/홀수_vs_짝수/Solution66.java
@@ -15,7 +15,13 @@ public class Solution66 {
     }
 
     public int solution(int[] num_list) {
-        int answer = 0;
-        return answer;
+        int sumOdd = 0, sumEven = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) sumOdd += num_list[i];
+            else sumEven += num_list[i];
+        }
+        
+        return Math.max(sumOdd, sumEven);
     }
 }
