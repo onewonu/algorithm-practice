8bd0559f2c47beeaa4d43548523794a8555c8149
wu <so222234@gmail.com>
2024-12-22 21:18:51 +0900

    :memo: Set Problem 'n보다 커질 때까지 더하기'

---
+++ basic/n_보다_커질_때까지_더하기/Solution69.java
@@ -0,0 +1,21 @@
+package basic.n_보다_커질_때까지_더하기;
+
+import java.util.Arrays;
+
+public class Solution69 {
+    public static void main(String[] args) {
+        Solution69 solution = new Solution69();
+        runTest(solution, new int[] {34, 5, 71, 29, 100, 34}, 123);
+        runTest(solution, new int[] {58, 44, 27, 10, 100}, 139);
+    }
+
+    private static void runTest(Solution69 solution, int[] numbers, int n) {
+        int result = solution.solution(numbers, n);
+        System.out.printf("solution(%s %d) = %d%n", Arrays.toString(numbers), n, result);
+    }
+
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
