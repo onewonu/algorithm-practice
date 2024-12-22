654cf348496bc648de62cab5c117bb7b3dc831de
wu <so222234@gmail.com>
2024-12-23 01:05:31 +0900

    :sparkles: Solution '수열과 구간 쿼리 1'

--- basic/수열과_구간_쿼리_1/Solution70.java
+++ basic/수열과_구간_쿼리_1/Solution70.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Solution70 {
     public static void main(String[] args) {
         Solution70 solution = new Solution70();
+        runTest(solution, new int[] { 0, 1, 2, 3, 4 }, new int[][]{ {0, 1}, {0, 1}, {1, 2}, {2, 3} });
         runTest(solution, new int[] { 0, 1, 2, 3, 4 }, new int[][]{ {0, 1}, {1, 2}, {2, 3} });
     }
 
@@ -14,10 +15,19 @@ public class Solution70 {
     }
 
     public int[] solution(int[] arr, int[][] queries) {
+        int[] diff = new int[arr.length + 1];
+
         for (int[] query : queries) {
-            for (int j = query[0]; j < query[1] + 1; j++) {
-                arr[j] += 1;
-            }
+            int start = query[0];
+            int end = query[1];
+            diff[start] += 1;
+            if (end + 1 < diff.length) diff[end + 1] -= 1;
+        }
+
+        int current = 0;
+        for (int i = 0; i < arr.length; i++) {
+            current += diff[i];
+            arr[i] += current;
         }
 
         return arr;
