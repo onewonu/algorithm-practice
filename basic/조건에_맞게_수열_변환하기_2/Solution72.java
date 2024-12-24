2fe0f0b26e8f7026e407f6e1247544c5c0e64521
Wonwoo Seo <wonwooseo2@gmail.com>
2024-12-24 17:07:16 +0900

    :construction: Resolving '조건에_맞게_수열_변환하기_2'-while

--- basic/조건에_맞게_수열_변환하기_2/Solution72.java
+++ basic/조건에_맞게_수열_변환하기_2/Solution72.java
@@ -14,7 +14,28 @@ public class Solution72 {
     }
 
     public int solution(int[] arr) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+
+        while (true) {
+            int[] newArr = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                    newArr[i] = arr[i] / 2;
+                } else if (arr[i] < 50 && arr[i] % 2 == 1) {
+                    newArr[i] = (arr[i] * 2) + 1;
+                } else {
+                    newArr[i] = arr[i];
+                }
+            }
+
+            if (Arrays.equals(arr, newArr)) break;
+
+            arr = newArr;
+
+            count++;
+        }
+
+        return count;
     }
 }
