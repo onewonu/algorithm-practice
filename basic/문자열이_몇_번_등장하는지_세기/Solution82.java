7311ca392615c43ac93735368132f04710ccb9cb
wu <so222234@gmail.com>
2025-01-03 01:08:10 +0900

    :construction: Resolving '문자열이 몇 번 등장하는지 세기'-for, indexOf

--- basic/문자열이_몇_번_등장하는지_세기/Solution82.java
+++ basic/문자열이_몇_번_등장하는지_세기/Solution82.java
@@ -1,5 +1,7 @@
 package basic.문자열이_몇_번_등장하는지_세기;
 
+import java.util.Arrays;
+
 public class Solution82 {
     public static void main(String[] args) {
         Solution82 solution = new Solution82();
@@ -14,6 +16,12 @@ public class Solution82 {
 
     public int solution(String myString, String pat) {
         int answer = 0;
+
+        for (int i = 0; i < myString.length(); i++) {
+            i = myString.indexOf(pat, i);
+            if (i == -1) return answer;
+            answer++;
+        }
         return answer;
     }
 }
