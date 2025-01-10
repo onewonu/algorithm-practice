e32bcf05f6e1378253de5bf0bad7b7d0cfcf7738
Wonwoo Seo <wonwooseo2@gmail.com>
2025-01-10 10:05:32 +0900

    :memo: Set Problem 'rny_string'

---
+++ basic/rny_string/Solution90.java
@@ -0,0 +1,21 @@
+package basic.rny_string;
+
+public class Solution90 {
+    public static void main(String[] args) {
+        Solution90 solution = new Solution90();
+        runTest(solution, "masterpiece");
+        runTest(solution, "programmers");
+        runTest(solution, "jerry");
+        runTest(solution, "burn");
+    }
+
+    private static void runTest(Solution90 solution, String rny_string) {
+        String result = solution.solution(rny_string);
+        System.out.printf("solution(%s) = %s%n", rny_string, result);
+    }
+
+    public String solution(String rny_string) {
+        String answer = "";
+        return answer;
+    }
+}
