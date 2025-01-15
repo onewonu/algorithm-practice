6c0317e9d6564b1d22e1f0d9028ccf3701043cf8
Wonwoo Seo <wonwooseo2@gmail.com>
2025-01-15 17:42:52 +0900

    :sparkles: Solution '배열의 길이를 2의 거듭제곱으로 만들기'-bit

--- basic/배열의_길이를_2의_거듭제곱으로_만들기/Solution96.java
+++ basic/배열의_길이를_2의_거듭제곱으로_만들기/Solution96.java
@@ -1,8 +1,6 @@
 package basic.배열의_길이를_2의_거듭제곱으로_만들기;
 
 import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 public class Solution96 {
     public static void main(String[] args) {
@@ -13,11 +11,22 @@ public class Solution96 {
 
     private static void runTest(Solution96 solution, int[] arr) {
         int[] result = solution.solution(arr);
-        System.out.printf("solution(%s, %d) = %s%n", Arrays.toString(arr), Arrays.toString(result));
+        System.out.printf("solution(%s) = %s%n", Arrays.toString(arr), Arrays.toString(result));
     }
 
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        int arrLength = arr.length;
+        if ((arrLength & (arrLength - 1)) == 0) {
+            return arr;
+        }
+
+        int nearestPowerOfTwo = 1;
+        while (nearestPowerOfTwo < arrLength) {
+            nearestPowerOfTwo <<= 1;
+        }
+
+        int[] answer = new int[nearestPowerOfTwo];
+        System.arraycopy(arr, 0, answer, 0, arrLength);
         return answer;
     }
 }
