7a8002cd9ab01bd7f1ecfe209247b56f727b21d7
Wonwoo Seo <wonwooseo2@gmail.com>
2025-01-16 18:04:10 +0900

    :construction: Resolving '배열 비교하기'

--- basic/배열_비교하기/Solution97.java
+++ basic/배열_비교하기/Solution97.java
@@ -16,7 +16,31 @@ public class Solution97 {
     }
 
     public int solution(int[] arr1, int[] arr2) {
-        int answer = 0;
-        return answer;
+        if (arr1.length != arr2.length) {
+            if (arr1.length > arr2.length) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else {
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i : arr1) {
+                sum1 += i;
+            }
+            for (int i : arr2) {
+                sum2 += i;
+            }
+            if (sum1 != sum2) {
+                if (sum1 > sum2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                return 0;
+            }
+        }
     }
 }
