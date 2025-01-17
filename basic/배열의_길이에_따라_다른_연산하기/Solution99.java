a2b8601f6aef216708f094fbacc74fae15f4479c
Wonwoo Seo <wonwooseo2@gmail.com>
2025-01-17 15:14:53 +0900

    :memo: Set Problem '배열의 길이에 따라 다른 연산하기'

---
+++ basic/배열의_길이에_따라_다른_연산하기/Solution99.java
@@ -0,0 +1,21 @@
+package basic.배열의_길이에_따라_다른_연산하기;
+
+import java.util.Arrays;
+
+public class Solution99 {
+    public static void main(String[] args) {
+        Solution99 solution = new Solution99();
+        runTest(solution, new int[] { 49, 12, 100, 276, 33 }, 27);
+        runTest(solution, new int[] { 444, 555, 666, 777 }, 100);
+    }
+
+    private static void runTest(Solution99 solution, int[] arr, int n) {
+        int[] result = solution.solution(arr, n);
+        System.out.printf("solution(%s, %d) = %s%n", Arrays.toString(arr), n, Arrays.toString(result));
+    }
+
+    public int[] solution(int[] arr, int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
