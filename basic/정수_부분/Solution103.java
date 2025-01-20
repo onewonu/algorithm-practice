153a376295eae147474ef5bd7062094ad019d9f6
Wonwoo Seo <wonwooseo2@gmail.com>
2025-01-20 14:43:40 +0900

    :memo: Set Problem '정수 부분'

---
+++ basic/정수_부분/Solution103.java
@@ -0,0 +1,19 @@
+package basic.정수_부분;
+
+public class Solution103 {
+    public static void main(String[] args) {
+        Solution103 solution = new Solution103();
+        runTest(solution, 1.42);
+        runTest(solution, 69.32);
+    }
+
+    private static void runTest(Solution103 solution, double flo) {
+        int result = solution.solution(flo);
+        System.out.printf("solution(%f) = %s%n", flo ,result);
+    }
+
+    public int solution(double flo) {
+        int answer = 0;
+        return answer;
+    }
+}
