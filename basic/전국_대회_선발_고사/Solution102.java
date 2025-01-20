c8a37c6550e778a14fd3178d10394a0a131fa544
Wonwoo Seo <wonwooseo2@gmail.com>
2025-01-20 14:10:55 +0900

    :sparkles: Solution '전국 대회 선발 고사'

--- basic/전국_대회_선발_고사/Solution102.java
+++ basic/전국_대회_선발_고사/Solution102.java
@@ -1,6 +1,9 @@
 package basic.전국_대회_선발_고사;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Solution102 {
     public static void main(String[] args) {
@@ -16,7 +19,17 @@ public class Solution102 {
     }
 
     public int solution(int[] rank, boolean[] attendance) {
-        int answer = 0;
-        return answer;
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < rank.length; i++) {
+            if (attendance[i]) {
+                list.add(new int[]{rank[i], i});
+            }
+        }
+        list.sort(Comparator.comparingInt(a -> a[0]));
+
+        int a = list.get(0)[1];
+        int b = list.get(1)[1];
+        int c = list.get(2)[1];
+        return 10000 * a + 100 * b + c;
     }
 }
