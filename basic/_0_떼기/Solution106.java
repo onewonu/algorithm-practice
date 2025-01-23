9961fba8a4a59aee9e8fa6edb1ae39e4e156e4e1
wu <so222234@gmail.com>
2025-01-24 00:50:34 +0900

    :memo: Set Problem '0 떼기'

---
+++ basic/_0_떼기/Solution106.java
@@ -0,0 +1,19 @@
+package basic._0_떼기;
+
+public class Solution106 {
+    public static void main(String[] args) {
+        Solution106 solution = new Solution106();
+        runTest(solution, "0010");
+        runTest(solution, "854020");
+    }
+
+    private static void runTest(Solution106 solution, String n_str) {
+        String result = solution.solution(n_str);
+        System.out.printf("solution(%s) = %s%n", n_str, result);
+    }
+
+    public String solution(String n_str) {
+        String answer = "";
+        return answer;
+    }
+}
