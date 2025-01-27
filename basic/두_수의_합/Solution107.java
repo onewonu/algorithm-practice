c41610a53b706567b1a8a9a4d3132e9058b583fb
wu <so222234@gmail.com>
2025-01-27 21:46:05 +0900

    :construction: Resolving '두 수의 합'-2번 테스트 케이스만 통과
    문제
    - repeat 고정 조건
    - 불필요한 조건문
    - 반복문 내부에서 모든것을 처리
    - 명확하지 않은 변수명

--- basic/두_수의_합/Solution107.java
+++ basic/두_수의_합/Solution107.java
@@ -14,24 +14,22 @@ public class Solution107 {
     }
 
     public String solution(String a, String b) {
-        // TODO: 타입을 초과하는 숫자가 주어졌음.
-        // TODO: 직접 올림으로 구현 방향성을 선택.
-
-        // TODO: 비교하는 두 수의 숫자의 길이가 다르다면 자리수를 맞춘다.
         String repeat = "";
         if (a.length() < b.length()) {
             repeat = "0".repeat(b.length() - a.length()) + a;
+        } else if (a.length() > b.length()) {
+            repeat = "0".repeat(a.length() - b.length()) + b;
         }
 
-        // TODO: 같은 자리수를 아스키값을 빼서 정수로 만들어 계산한다. 이때, 올림을 유의한다.
-        // TODO: 시작인덱스 역순 부터
         int carry = 0;
-        for (int i = repeat.length() - 1; i == 0; i--) {
-        //for (int i = 0; i < 5; i++) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = repeat.length() - 1; i >= 0; i--) {
             int a1 = 0;
             int b1 = 0;
+            int sum;
+            int remainder;
 
-            if (a.charAt(i) != 0) {
+            if (repeat.charAt(i) != 0) {
                 a1 = repeat.charAt(i) - '0';
             }
 
@@ -39,13 +37,18 @@ public class Solution107 {
                 b1 = b.charAt(i) - '0';
             }
 
-            int sum = a1 + b1;
-            if (sum > 10) {
+            sum = a1 + b1 + carry;
+            carry = 0;
+
+            if (sum > 9) {
                 carry = sum / 10;
-                int test = sum % 10;
+                remainder = sum % 10;
+                answer.append(remainder);
+            } else {
+                answer.append(sum);
             }
         }
 
-        return "";
+        return answer.reverse().toString();
     }
 }
