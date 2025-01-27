e6adc232d4083ac6cd22d999d227587defb2c781
wu <so222234@gmail.com>
2025-01-27 22:42:35 +0900

    :memo: Set Problem '문자열로 변환'

---
+++ basic/문자열로_변환/Solution108.java
@@ -0,0 +1,19 @@
+package basic.문자열로_변환;
+
+public class Solution108 {
+    public static void main(String[] args) {
+        Solution108 solution = new Solution108();
+        runTest(solution, 123);
+        runTest(solution, 2573);
+    }
+
+    private static void runTest(Solution108 solution, int n) {
+        String result = solution.solution(n);
+        System.out.printf("solution(%d) = %s%n", n, result);
+    }
+
+    public String solution(int n) {
+        String answer = "";
+        return answer;
+    }
+}
