daf45ee711092464be628934dac7836e3c4c8f34
wu <so222234@gmail.com>
2025-01-27 22:26:39 +0900

    :sparkles: Solution '두 수의 합'-repeat, carry, stringBuilder

--- basic/두_수의_합/Solution107.java
+++ basic/두_수의_합/Solution107.java
@@ -3,9 +3,9 @@ package basic.두_수의_합;
 public class Solution107 {
     public static void main(String[] args) {
         Solution107 solution = new Solution107();
-        //runTest(solution, "582", "734");
+        runTest(solution, "582", "734");
         runTest(solution, "18446744073709551615", "287346502836570928366");
-        //runTest(solution, "0", "0");
+        runTest(solution, "0", "0");
     }
 
     private static void runTest(Solution107 solution, String a, String b) {
@@ -14,41 +14,31 @@ public class Solution107 {
     }
 
     public String solution(String a, String b) {
-        String repeat = "";
-        if (a.length() < b.length()) {
-            repeat = "0".repeat(b.length() - a.length()) + a;
-        } else if (a.length() > b.length()) {
-            repeat = "0".repeat(a.length() - b.length()) + b;
-        }
+        StringBuilder answer = new StringBuilder();
+
+        int maxLength = Math.max(a.length(), b.length());
+        a = alignWithZeros(a, maxLength);
+        b = alignWithZeros(b, maxLength);
 
         int carry = 0;
-        StringBuilder answer = new StringBuilder();
-        for (int i = repeat.length() - 1; i >= 0; i--) {
-            int a1 = 0;
-            int b1 = 0;
-            int sum;
-            int remainder;
 
-            if (repeat.charAt(i) != 0) {
-                a1 = repeat.charAt(i) - '0';
-            }
+        for (int i = maxLength - 1; i >= 0; i--) {
+            int digitA = a.charAt(i) - '0';
+            int digitB = b.charAt(i) - '0';
+            int sum = digitA + digitB + carry;
 
-            if (b.charAt(i) != 0) {
-                b1 = b.charAt(i) - '0';
-            }
+            carry = sum / 10;
+            answer.append(sum % 10);
+        }
 
-            sum = a1 + b1 + carry;
-            carry = 0;
-
-            if (sum > 9) {
-                carry = sum / 10;
-                remainder = sum % 10;
-                answer.append(remainder);
-            } else {
-                answer.append(sum);
-            }
+        if (carry > 0) {
+            answer.append(carry);
         }
 
         return answer.reverse().toString();
+    }
+
+    private String alignWithZeros(String str, int maxLength) {
+        return "0".repeat(maxLength - str.length()) + str;
     }
 }
