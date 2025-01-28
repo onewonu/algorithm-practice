5e7be550613875270cb0f0fa7b86e72ab797821a
wu <so222234@gmail.com>
2025-01-28 18:25:55 +0900

    :memo: Set Problem '부분 문자열인지 확인하기'

---
+++ basic/부분_문자열인지_확인하기/Solution110.java
@@ -0,0 +1,19 @@
+package basic.부분_문자열인지_확인하기;
+
+public class Solution110 {
+    public static void main(String[] args) {
+        Solution110 solution = new Solution110();
+        runTest(solution, "banana", "ana");
+        runTest(solution, "banana", "wxyz");
+    }
+
+    private static void runTest(Solution110 solution, String my_string, String target) {
+        int result = solution.solution(my_string, target);
+        System.out.printf("solution(%s, %s) = %d%n", my_string, target, result);
+    }
+
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        return answer;
+    }
+}
