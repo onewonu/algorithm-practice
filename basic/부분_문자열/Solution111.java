5ca2b53c9305b01f41efc6103dee16c5a22dfad4
wu <so222234@gmail.com>
2025-01-28 18:43:07 +0900

    :memo: Set Problem '부분 문자열'

---
+++ basic/부분_문자열/Solution111.java
@@ -0,0 +1,19 @@
+package basic.부분_문자열;
+
+public class Solution111 {
+    public static void main(String[] args) {
+        Solution111 solution = new Solution111();
+        runTest(solution, "abc", "aabcc");
+        runTest(solution, "tbt", "tbbttb");
+    }
+
+    private static void runTest(Solution111 solution, String str1, String str2) {
+        int result = solution.solution(str1, str2);
+        System.out.printf("solution(%s, %s) = %d%n", str1, str2, result);
+    }
+
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
+}
