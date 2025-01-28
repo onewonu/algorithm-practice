c0d988250118e9f9d024dee8a97ac3fc645226e2
wu <so222234@gmail.com>
2025-01-28 17:24:50 +0900

    :memo: Set Problem '배열의 원소 삭제하기'

---
+++ basic/배열의_원소_삭제하기/Solution109.java
@@ -0,0 +1,21 @@
+package basic.배열의_원소_삭제하기;
+
+import java.util.Arrays;
+
+public class Solution109 {
+    public static void main(String[] args) {
+        Solution109 solution = new Solution109();
+        runTest(solution, new int[] { 293, 1000, 395, 678, 94 }, new int[] { 94, 777, 104, 1000, 1, 12 });
+        runTest(solution, new int[] { 110, 66, 439, 785, 1 }, new int[] { 377, 823, 119, 43 });
+    }
+
+    private static void runTest(Solution109 solution, int[] arr, int[] delete_list) {
+        int[] result = solution.solution(arr, delete_list);
+        System.out.printf("solution(%s, %s) = %s%n", Arrays.toString(arr), Arrays.toString(delete_list), Arrays.toString(result));
+    }
+
+    public int[] solution(int[] arr, int[] delete_list) {
+        int[] answer = {};
+        return answer;
+    }
+}
