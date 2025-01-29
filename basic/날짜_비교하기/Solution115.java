c37a41c666f6bcd49358985c6cc35201a23ffb6a
wu <so222234@gmail.com>
2025-01-29 16:04:34 +0900

    :memo: Set Problem '날짜 비교하기'

---
+++ basic/날짜_비교하기/Solution115.java
@@ -0,0 +1,21 @@
+package basic.날짜_비교하기;
+
+import java.util.Arrays;
+
+public class Solution115 {
+    public static void main(String[] args) {
+        Solution115 solution = new Solution115();
+        runTest(solution, new int[] { 2021, 12, 28 }, new int[] { 2021, 12, 29 });
+        runTest(solution, new int[] { 1024, 10, 24 }, new int[] { 1024, 10, 24 });
+    }
+
+    private static void runTest(Solution115 solution, int[] date1, int[] date2) {
+        int result = solution.solution(date1, date2);
+        System.out.printf("solution(%s, %s) = %d%n", Arrays.toString(date1), Arrays.toString(date2), result);
+    }
+
+    public int solution(int[] date1, int[] date2) {
+        int answer = 0;
+        return answer;
+    }
+}
