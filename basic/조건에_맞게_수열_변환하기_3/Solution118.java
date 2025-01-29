907a60e1ed7b2d799b2fc075cae621c16f3e72f5
wu <so222234@gmail.com>
2025-01-30 05:29:32 +0900

    :memo: Set Problem '조건에 맞게 수열 변환하기 3'

---
+++ basic/조건에_맞게_수열_변환하기_3/Solution118.java
@@ -0,0 +1,21 @@
+package basic.조건에_맞게_수열_변환하기_3;
+
+import java.util.Arrays;
+
+public class Solution118 {
+    public static void main(String[] args) {
+        Solution118 solution = new Solution118();
+        runTest(solution, new int[]{ 1, 2, 3, 100, 99, 98 }, 3);
+        runTest(solution, new int[]{ 1, 2, 3, 100, 99, 98 }, 2);
+    }
+
+    private static void runTest(Solution118 solution, int[] arr, int k) {
+        int[] result = solution.solution(arr, k);
+        System.out.printf("solution(%s, %d) = %s%n", Arrays.toString(arr), k, Arrays.toString(result));
+    }
+
+    public int[] solution(int[] arr, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
