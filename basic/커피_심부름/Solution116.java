585883fd21983beeb04371ed6c579d45206682e4
wu <so222234@gmail.com>
2025-01-29 16:29:48 +0900

    :memo: Set Problem '커피 심부름'

---
+++ basic/커피_심부름/Solution116.java
@@ -0,0 +1,21 @@
+package basic.커피_심부름;
+
+import java.util.Arrays;
+
+public class Solution116 {
+    public static void main(String[] args) {
+        Solution116 solution = new Solution116();
+        runTest(solution, new String[] { "cafelatte", "americanoice", "hotcafelatte", "anything" });
+        runTest(solution, new String[] { "americanoice", "americano", "iceamericano" });
+    }
+
+    private static void runTest(Solution116 solution, String[] order) {
+        int result = solution.solution(order);
+        System.out.printf("solution(%s) = %d%n", Arrays.toString(order), result);
+    }
+
+    public int solution(String[] order) {
+        int answer = 0;
+        return answer;
+    }
+}
