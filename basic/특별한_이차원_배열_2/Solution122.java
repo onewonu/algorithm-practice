4b4154d600ecf018f860857f9dd6bb1a59e54877
wu <so222234@gmail.com>
2025-01-30 22:37:39 +0900

    :sparkles: Solution '특별한 이차원 배열 2'

--- basic/특별한_이차원_배열_2/Solution122.java
+++ basic/특별한_이차원_배열_2/Solution122.java
@@ -15,7 +15,14 @@ public class Solution122 {
     }
 
     public int solution(int[][] arr) {
-        int answer = 0;
-        return answer;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                   return 0;
+                }
+            }
+        }
+
+        return 1;
     }
 }
