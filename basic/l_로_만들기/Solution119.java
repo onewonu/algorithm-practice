68b0f2ce78a5db223b10830468dd2d8aefdaed4b
wu <so222234@gmail.com>
2025-01-30 16:00:28 +0900

    :memo: Set Problem 'l 로 만들기'

---
+++ basic/l_로_만들기/Solution119.java
@@ -0,0 +1,19 @@
+package basic.l_로_만들기;
+
+public class Solution119 {
+    public static void main(String[] args) {
+        Solution119 solution = new Solution119();
+        runTest(solution, "abcdevwxyz");
+        runTest(solution, "jjnnllkkmm");
+    }
+
+    private static void runTest(Solution119 solution, String myString) {
+        String result = solution.solution(myString);
+        System.out.printf("solution(%s) = %s%n", myString, result);
+    }
+
+    public String solution(String myString) {
+        String answer = "";
+        return answer;
+    }
+}
