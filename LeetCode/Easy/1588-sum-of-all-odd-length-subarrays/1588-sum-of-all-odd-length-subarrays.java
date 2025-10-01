f835a0d205c116b33ffec58469a4a3fc70a3e7d9
Wonwoo-Seo <so222234@gmail.com>
2025-10-01 15:44:15 +0900

    1588-sum-of-all-odd-length-subarrays Difficulty: Easy  Time: 0 ms (100%), Space: 41.3 MB (78.71%) - LeetHub

--- LeetCode/Easy/1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ LeetCode/Easy/1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,27 +1,18 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
         int len = arr.length;
         
-        int oddarr[] = new int[(len + 1) / 2];
-        int index = 0;
-        for (int i = 1; i <= len; i += 2) {
-            oddarr[index] = i;
-            index++;
+        for (int i = 0; i < len; i++) {
+            int left = i + 1;        
+            int right = len - i;       
+            int total = left * right; 
+            
+            int oddCount = (total + 1) / 2;
+            
+            sum += arr[i] * oddCount;
         }
-
-        int sum = 0;
-        int partSum = 0;
-        for (int odd : oddarr) {
-            for (int i = 0; i <= len - odd; i++) {
-                for (int j = i; j < i + odd; j++) {
-                    partSum += arr[j];
-                }
-                
-                sum += partSum;
-                partSum = 0;
-            }
-        }
-
+        
         return sum;
     }
 }
