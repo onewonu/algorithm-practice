c1f3df9dff3e491c6f97dc6d26f5aaf6b2d8fe86
Wonwoo-Seo <so222234@gmail.com>
2025-10-02 15:03:17 +0900

    1732-find-the-highest-altitude Difficulty: Easy  Time: 0 ms (100%), Space: 41.3 MB (58.51%) - LeetHub

--- LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,19 +1,11 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int len = gain.length;
-        int prefix[] = new int[len + 1];
+        int current = 0;
+        int max = 0;
 
-        int index = 0;
-        for (int g : gain) {
-            prefix[index + 1] = prefix[index] + gain[index];
-            index++;
-        }
-
-        int max = prefix[0];
-        for (int p : prefix) {
-            if (p > max) {
-                max = p;
-            }
+        for (int num : gain) {
+            current += num;
+            max = Math.max(max, current);
         }
 
         return max;
