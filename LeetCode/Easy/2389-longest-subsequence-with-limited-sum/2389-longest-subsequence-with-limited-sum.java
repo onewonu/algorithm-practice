18eb6ca6ce4240a284c6f43f336a0b0ea36b8cfe
Wonwoo-Seo <so222234@gmail.com>
2025-10-05 15:58:18 +0900

    2389-longest-subsequence-with-limited-sum Difficulty: Easy  Time: 6 ms (53.32%), Space: 45.3 MB (36.27%) - LeetHub

--- LeetCode/Easy/2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ LeetCode/Easy/2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
+        int[] result = new int[queries.length];
+        
         Arrays.sort(nums);
         
         int[] prefix = new int[nums.length + 1];
@@ -7,18 +9,25 @@ class Solution {
             prefix[i + 1] = prefix[i] + nums[i];
         }
         
-        int[] result = new int[queries.length];
-        
         for (int i = 0; i < queries.length; i++) {
-            for (int j = 0; j < prefix.length; j++) {
-                if (queries[i] >= prefix[j]) {
-                    result[i] = j;
+            int left = 0;
+            int right = prefix.length - 1;
+            int answer = 0;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (prefix[mid] <= queries[i]) {
+                    answer = mid;
+                    left = mid + 1;
                 } else {
-                    break;
+                    right = mid - 1;
                 }
             }
+
+            result[i] = answer;
         }
-        
+
         return result;
     }
 }
