15f09b0429a7f40d813c8e25c8ace1bf676a0104
Wonwoo-Seo <so222234@gmail.com>
2025-10-20 11:27:45 +0900

    1791-find-center-of-star-graph Difficulty: Easy  Time: 0 ms (100%), Space: 72.2 MB (67.72%) - LeetHub

--- LeetCode/Easy/1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ LeetCode/Easy/1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,29 +1,8 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-
-        for (int[] edge : edges) {
-            for (int num : edge) {
-                int newCount = countMap.getOrDefault(num, 0) + 1;
-                countMap.put(num, newCount);
-
-                // 중심 노드가 모든 간선에 연결 됨, 중심 노드의 차수 = n - 1 = edges.length
-                if (newCount == edges.length) {
-                    return num;
-                }
-            }
-        }
-
-        int mostKey = 0;
-        int maxCount = 0;
-
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                mostKey = entry.getKey();
-            }
-        }
-
-        return mostKey;
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) 
+            return edges[0][0];
+        else 
+            return edges[0][1];
     }
 }
