49c028ed7572203bb18bc391d3d59178ecb38209
Wonwoo-Seo <so222234@gmail.com>
2026-04-11 10:03:33 +0900

    0872-leaf-similar-trees Difficulty: Easy  Time: 0 ms (100%), Space: 43.2 MB (58.56%) - LeetHub

---
+++ LeetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> result1 = new ArrayList<>();
+        List<Integer> result2 = new ArrayList<>();
+
+        dfs(root1, result1);
+        dfs(root2, result2);
+
+        return result1.equals(result2);
+    }
+
+    private void dfs(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.left == null && node.right == null) {
+            list.add(node.val);
+        }
+
+        dfs(node.left, list);  
+        dfs(node.right, list);
+    }
+}
